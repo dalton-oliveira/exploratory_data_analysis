053ffd71a96cf361c1bc27d0a9726f88d198a235
darto <dalton.oliveira.84@gmail.com>
2019-02-06 12:24:43 -0200

    code duplication removed. Created data directory

--- plot4.R
+++ plot4.R
@@ -1,16 +1,4 @@
-library(data.table)
-library(lubridate)
-options(digits = 16)
-
-if (!file.exists("data/household_power_consumption.txt")) {
-  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile = "data/household_power_consumption.zip")
-  unzip(zipfile = "data/household_power_consumption.zip", exdir = "data/")
-  file.remove("data/household_power_consumption.zip")
-}
-
-df <- fread("data/household_power_consumption.txt", sep=";", header = TRUE, na.strings = "?", colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
-df <- subset(df,df$Date=="1/2/2007" | df$Date == "2/2/2007")
-df$DateTime <- dmy_hms(paste(df$Date, df$Time))
+source("loadDataset.R")
 
 png(file = "plot4.png")
 
